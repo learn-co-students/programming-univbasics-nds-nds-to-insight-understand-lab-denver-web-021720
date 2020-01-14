243643c4e55fc80adb32bae1b131bd141bc45e04
Joe Gelay <joegelay@gmail.com>
2020-01-14 20:41:40 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,15 +1,28 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
+require 'pry'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp nds
-  puts pp nds
 end
 
 def print_first_directors_movie_titles
   
+  # spielberg_movies is an array of Spielberg's movies (hashes)...
+  spielberg_movies = directors_database[0][:movies]
+  # puts spielberg_movies
   
+  # loop outputs title of each movie hash 
+  counter = 0
+  while counter < spielberg_movies.length do
+    puts spielberg_movies[counter][:title]
+    
+    counter += 1 
+    
+  end
 end
+
+print_first_directors_movie_titles
