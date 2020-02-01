c7cd1c3b469de5362d756f94185b5e619da48087
Kristine Du <kristine.a.du@gmail.com>
2020-02-01 19:50:34 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,22 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
+pretty_print_nds(directors_database)
+
 def print_first_directors_movie_titles
+  # AoHoAoH
+  # Outer array - First director is in 0 position
+  # Hash of outer array - key is :movies
+  # Loop through all inner array positions
+  # Hash of inner array - key is :title 
+  inner_index = 0
+  while inner_index < directors_database[0][:movies].length do
+    puts "#{directors_database[0][:movies][inner_index][:title]}\n"
+    inner_index += 1
+  end
 end
+
+print_first_directors_movie_titles
